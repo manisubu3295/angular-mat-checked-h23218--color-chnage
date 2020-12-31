b6f7e9f0e30a190e87f72c0175186249fa9d17bd
manisubu3295 <manisubu3295@gmail.com>
2020-12-31 09:01:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mat-checked-h23218
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mat-checked-h23218)
---
+++ app/checkbox-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+/**
+ * @title Basic checkboxes
+ */
+@Component({
+  selector: 'checkbox-overview-example',
+  templateUrl: 'checkbox-overview-example.html',
+  encapsulation: ViewEncapsulation.None,
+})
+export class CheckboxOverviewExample {}
+
+
+/**  Copyright 2017 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
